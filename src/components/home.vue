5933d3c681a657112bf2f8cc869c3d99659f955a
13112408234 <2265120248@qq.com>
2021-05-28 17:14:39 +0800

    请求添加token

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,8 +1,48 @@
 <template>
-  <div>
-    <div>首页</div>
-    <el-button @click="logout" class="el-button-logout">退出</el-button>
-  </div>
+  <el-container class="home-container">
+    <!-- 头部区 -->
+    <el-header>
+      <div>
+        <img src="" alt="" />
+        <span>后台管理系统</span>
+      </div>
+      <el-button @click="logout" class="el-button-logout">退出</el-button>
+    </el-header>
+    <!-- 页面主体区 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside width="200px">
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+        >
+          <!-- 一级菜单 -->
+          <el-submenu index="1">
+            <!-- 一级菜单模板区域 -->
+            <template slot="title">
+              <!-- 图标 -->
+              <i class="el-icon-location"></i>
+              <!-- 文本 -->
+              <span>导航一</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item index="1-4-1">
+              <!-- 二级菜单模板区域 -->
+              <template slot="title">
+                <!-- 图标 -->
+                <i class="el-icon-location"></i>
+                <!-- 文本 -->
+                <span>导航一</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 右侧内容主题 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 <script>
 export default {
@@ -15,9 +55,23 @@ export default {
 };
 </script>
 <style scoped>
-.el-button-logout {
-  position: absolute;
-  right: 20px;
-  top: 10px;
+.home-container {
+  height: 100%;
+}
+.el-header {
+  color: #fff;
+  font-size: 30px;
+  background-color: #373d41;
+  /* 左右对齐 */
+  display: flex;
+  justify-content: space-between;
+  /* 退出按钮垂直居中 */
+  align-items: center;
+}
+.el-aside {
+  background-color: #333744;
+}
+.el-main {
+  background-color: #eaedf1;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -13,6 +13,12 @@ axios.defaults.baseURL = 'http://www.timemeetyou.com:8889/api/private/v1'
 //把axios对象赋值给原型链，在全部组件中可以通过this.$http访问
 Vue.prototype.$http = axios
 
+//后端设置了权限，每一次请求都需要添加token，使用axios拦截器在数据发送请求前添加token
+axios.interceptors.request.use(config => {
+  console.log(config)
+  config.headers.Authorization = window.sessionStorage.getItem("token")
+  return config
+})
 
 Vue.use(ElementUI);
 Vue.config.productionTip = false
